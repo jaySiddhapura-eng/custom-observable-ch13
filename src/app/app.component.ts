70bc8b671b62755bc7f1949c2d9b3a2f43ccb936
Siddhapura <jay.siddhapura@cgi.com>
2020-07-24 12:32:31 +0200

    Operator added using pipe 1. map 2. filter
    subject added in user.service.ts
    subject as a replacement for event emitter in service
    next method is being used on subject instead of emit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  userActivated = false;
+  private activatedSub: Subscription;
+
+  constructor(private userSer : UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userSer.activatedEmitter.subscribe(
+     (didAct:boolean) => {
+       this.userActivated = didAct;
+     }
+    );
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe();
+  }
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit,  OnDestroy} from '@angular/core';
 
 import {interval, Subscription, Observable} from 'rxjs' ;
 
+// importing operator as follow
+import {map, filter} from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -26,6 +30,9 @@ export class HomeComponent implements OnInit, OnDestroy{
         let count = 0;
         setInterval(()=>{
           observer.next(count);
+          if (count === 2){
+            observer.complete();
+          }
           if(count > 3){
             observer.error(new Error('count is greater then 3'));
           }
@@ -33,14 +40,35 @@ export class HomeComponent implements OnInit, OnDestroy{
       }
     );
 
-    this.Subscription = customIntervalObservable.subscribe(
+    // operator is performed on the observable which was iplemented above
+    let transformedObservableOutput = customIntervalObservable.pipe(
+      //transforming the items emitted by an Observable by applying a function to each item
+      map((data: number) =>{
+      let returnString = 'round: ' + (data+1);  // this is the function
+      return returnString;
+      }),
+
+      //emit only those items from an Observable that pass a predicted test
+      filter((data:number) => {
+        return data > 0;  // this is the test
+      })
+    );
+
+    this.Subscription = transformedObservableOutput.subscribe(
+      // received from data observable // later from transformed observable   
       data => {
         console.log(data);
       },
 
+      // received from error observable 
       error =>{
         console.log(error);
         alert(error.message);
+      },
+
+      // obtained from completion observable
+      () => {
+        console.log('completed');
       }
     );
 
